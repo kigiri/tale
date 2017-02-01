894399ed8b0807a5a4be08d30ce29653b692f623
Chookapic <chawkibarnat42@gmail.com>
2017-02-01 22:58:25 +0000

    add tales model

---
+++ db/tales.js
@@ -0,0 +1,36 @@
+const  {
+  text,
+  char,
+  serial,
+  timestamp,
+} = require('../lib/db-test-tools')
+
+const tale = {
+  
+  id: serial({
+    locked: true,
+    notNull: true,
+  }),
+
+  title: char(2, 80, {
+    exemple: 'The boy who liked apples',
+    notNull: true,
+  }),
+
+  author: {
+    ref: 'users.id',
+    notNull: true,
+  },
+
+  content: text({
+    exemple: `Once upon a time in far far east land,
+      bla bla bla and bla bla bla...
+      and they divorced.`,
+  }),
+  
+  modifiedAt: timestamp(),
+
+  createdAt: timestamp({ locked: true }),
+}
+
+module.exports = tale
