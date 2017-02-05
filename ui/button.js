262b0ea5905bdd9f47ea91503feb864fdf0aaff9
Chookapic <chawkibarnat42@gmail.com>
2017-02-05 23:29:28 +0000

    look ma no "a"

--- ui/button.js
+++ ui/button.js
@@ -1,10 +1,12 @@
 const h = require('../lib/h')
 const { div, a } = require('../lib/h')
 const block = h('.block')
-const button = h('.button')
+const button = h('a.button')
 const blue = button.extend('.is-info')
 const green = button.extend('.is-success')
 const red = button.extend('.is-danger')
 const yellow = button.extend('.is-warning')
 
-module.exports = content => a(blue(content))
+
+
+module.exports = content => blue(content)
